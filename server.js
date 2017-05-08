bc97cdec0d1228857942c90ed69c5337b47ebff8
Alan Matkorski <alanmatkorski@gmail.com>
2017-05-08 15:15:08 -0300

    Creo una vista nueva para el mapa

--- server.js
+++ server.js
@@ -8,13 +8,16 @@ var port = process.env.PORT || 8080;
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/app'));
 
-// set the home page route
-app.get('/', function(req, res) {
-
-    // make sure index is in the right directory. In this case /app/index.html
+app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.listen(port, function() {
+app.get('/map', function (req, res) {
+    // Hasta que decidamos que render engine vamos a usar devolvemos las vistas asi
+    res.sendFile(__dirname + '/app/map.html');
+});
+
+
+app.listen(port, function () {
     console.log('Our app is running on http://localhost:' + port);
 });
