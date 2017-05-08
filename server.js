0d966fe12894ad1d575887832ef2823fabdce044
Alan Matkorski <alanmatkorski@gmail.com>
2017-05-08 15:25:54 -0300

    integro el view render engine ejs

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/app'));
+app.set('views', './app');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('index');
 });
 
 app.get('/map', function (req, res) {
-    // Hasta que decidamos que render engine vamos a usar devolvemos las vistas asi
-    res.sendFile(__dirname + '/app/map.html');
+    res.render('map');
 });
 
 
