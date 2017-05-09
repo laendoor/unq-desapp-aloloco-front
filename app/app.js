d5917d36c0bb4cb9d2a416cb8c57cf82432094af
Alan Matkorski <alanmatkorski@gmail.com>
2017-05-09 20:49:11 -0300

    Agrega listado de cosas con angular

--- app/app.js
+++ app/app.js
@@ -3,9 +3,39 @@ var app = angular.module("aloloco", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl : "views/home.html"
+            templateUrl : "views/home.html",
+            controller: "HomeController"
+
         })
         .when("/map", {
             templateUrl : "views/map.html"
         });
 });
+
+app.controller('HomeController', ['$scope', function($scope) {
+
+  $scope.products = [
+  	{
+  		"id": 1,  		
+  		"name": "Papas Fritas",  		
+  		"brand": "Lays",  		
+  		"price": "20",  		
+  		"image": "http://2.bp.blogspot.com/_tQVckLGToNA/TB_rn9LxlZI/AAAAAAAAEjk/mpV31bXbmgU/s1600/LAYS_Classic.gif",  		
+  	},
+  	{
+  		"id": 2,  		
+  		"name": "Papas Fritas",  		
+  		"brand": "Lays",  		
+  		"price": "20",  		
+  		"image": "http://2.bp.blogspot.com/_tQVckLGToNA/TB_rn9LxlZI/AAAAAAAAEjk/mpV31bXbmgU/s1600/LAYS_Classic.gif",  		
+  	},
+  	{
+  		"id": 3,  		
+  		"name": "Papas Fritas",  		
+  		"brand": "Lays",  		
+  		"price": "20",  		
+  		"image": "http://2.bp.blogspot.com/_tQVckLGToNA/TB_rn9LxlZI/AAAAAAAAEjk/mpV31bXbmgU/s1600/LAYS_Classic.gif",  		
+  	},
+  ];
+
+}]);
