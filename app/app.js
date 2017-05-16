a4b8a4527dd146242375af63ff3db54cd29cda45
Alan Matkorski <alanmatkorski@gmail.com>
2017-05-16 16:33:41 -0300

    Agrega funcionalidad a la creacion de listas

--- app/app.js
+++ app/app.js
@@ -8,7 +8,8 @@ app.config(function ($routeProvider) {
 
         })
         .when("/users/:userId/wishlists/create", {
-            templateUrl: "views/wishlist-form.html"
+            templateUrl: "views/wishlist-form.html",
+            controller: "WishlistCreationController"
         })
         .when("/users/:userId/wishlists/:wishlistId", {
             templateUrl: "views/wishlist.html",
@@ -28,12 +29,29 @@ app.config(function (RestangularProvider) {
 
 app.controller('HomeController', ['$scope', 'Restangular', function ($scope, Restangular) {
 
+}]);
+
+app.controller('WishlistCreationController', ['$scope', 'Restangular', function ($scope, Restangular) {
+
     Restangular.all('stock')
         .customGET()
-        .then(function (products) {
-            $scope.products = products['data'];
+        .then(function (stock) {
+            $scope.stock = stock['data'];
         });
 
+    $scope.selectedProducts = [];
+
+    $scope.addProductToSelectedOnes = function (id) {
+        var product = $scope.stock.find(function (product) {
+            return product.id == id;
+        });
+
+        $scope.selectedProducts.push(product)
+
+        $scope.stock = $scope.stock.filter(function (product) {
+            return product.id != id;
+        })
+    }
 }]);
 
 app.controller('WishlistController', ['$scope', '$routeParams', function ($scope, $routeParams) {
