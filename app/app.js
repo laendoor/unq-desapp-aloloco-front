77a12d71e8e9bd699744714f8458483d2ea8e10a
Alan Matkorski <alanmatkorski@gmail.com>
2017-05-19 14:38:05 -0300

    cambio datos estaticos a llamadas angular

--- app/app.js
+++ app/app.js
@@ -16,7 +16,8 @@ app.config(function ($routeProvider) {
             controller: "WishlistController"
         })
         .when("/map", {
-            templateUrl: "views/map.html"
+            templateUrl: "views/map.html",
+            controller: "MapController"
         })
         .when("/admin/import", {
             templateUrl: "views/import.html",
@@ -33,12 +34,61 @@ app.config(function (RestangularProvider) {
 
 app.controller('HomeController', ['$scope', 'Restangular', function ($scope, Restangular) {
 
+    Restangular.all('client/wishlists').customGET().then(function (wishlists) {
+        $scope.wishlists = wishlists['data'];
+        console.log(wishlists['data']);
+    });
+
+}]);
+
+app.controller('MapController', ['$scope', function ($scope) {
+
+    $scope.initMap = function () {
+        var markerArray = [];
+
+        var directionsService = new google.maps.DirectionsService;
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: {lat: -34.7101435, lng: -58.2858949}
+        });
+
+        var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+
+        var stepDisplay = new google.maps.InfoWindow;
+
+        $scope.calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
+    }
+
+    $scope.calculateAndDisplayRoute = function (directionsDisplay, directionsService, markerArray, stepDisplay, map) {
+        for (var i = 0; i < markerArray.length; i++) {
+            markerArray[i].setMap(null);
+        }
+
+        directionsService.route({
+            origin: 'universidad de quilmes',
+            destination: 'estacion de bernal',
+            avoidTolls: true,
+            travelMode: google.maps.TravelMode.WALKING
+        }, function (response, status) {
+            if (status === google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+                document.getElementById('distance').innerHTML = response.routes[0].legs[0].distance.text;
+            } else {
+                window.alert('Directions request failed due to ' + status);
+            }
+        });
+    }
+
+    $scope.initMap();
+
 }]);
 
 app.controller('WishlistCreationController', ['$scope', 'Restangular', function ($scope, Restangular) {
 
     Restangular.all('stock').customGET().then(function (stock) {
         $scope.stock = stock['data'];
+        console.log(stock['data']);
     });
 
     $scope.selectedProducts = [];
@@ -48,6 +98,7 @@ app.controller('WishlistCreationController', ['$scope', 'Restangular', function 
             return product.id == id;
         });
 
+        product.amount = 10; // Hacerlo dinamico
         $scope.selectedProducts.push(product)
 
         $scope.stock = $scope.stock.filter(function (product) {
@@ -69,7 +120,7 @@ app.controller('ImportController', ['$scope', '$http', function ($scope, $http) 
 
     $scope.uploadFile = function () {
         $.ajax({
-            url: 'http://v3core.app/aloloco',
+            url: 'http://api.aloloco.dev/stock',
             type: 'POST',
             data: new FormData($('.uploader-form')[0]),
             cache: false,
@@ -77,5 +128,5 @@ app.controller('ImportController', ['$scope', '$http', function ($scope, $http) 
             processData: false,
         });
     }
-    
+
 }]);
---
+++ app/printvars.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+echo "<script>var api_route = '$HOME';</script>" > vars.js
