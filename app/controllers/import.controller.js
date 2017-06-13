fff7c854165642a8a03ccc83002fa52f6950b874
Alan Matkorski <alanmatkorski@gmail.com>
2017-06-13 18:00:30 -0300

    fix

--- app/controllers/import.controller.js
+++ app/controllers/import.controller.js
@@ -2,7 +2,7 @@ app.controller('ImportController', ['$scope', '$http', function ($scope, $http) 
 
     $scope.uploadFile = function () {
         $.ajax({
-            url: API_ROUTE + '/stock',
+            url: API_ROUTE + 'stock',
             type: 'POST',
             data: new FormData($('.uploader-form')[0]),
             cache: false,
